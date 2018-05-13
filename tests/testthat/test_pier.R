d044ab3b1d914ac1ca0437b4154ec39d8cfaaa0c
Matt Johnson <mrjoh3@gmail.com>
2018-05-13 21:17:16 +1000

    updating docs slowly adding unit tests

--- tests/testthat/test_pier.R
+++ tests/testthat/test_pier.R
@@ -4,10 +4,6 @@ pd <- data.frame(label = c('Environment','Education','Business','Community'),
                  color = c("#D7191C", "#FDAE61", "#ABDDA4", "#2B83BA"))
 
 p <- pier(pd)
-p_size <- pie.size(p, outer = 80, inner = 50, width = 200, height = 200)
-p_header <- pie.header(p, text = 'My New Title', font = 'Impact',
-                       size = 18, colour = 'grey',
-                       location = 'top-center')
 
 test_that("a simple pier works", {
     expect_is(p, "pier")
@@ -15,6 +11,9 @@ test_that("a simple pier works", {
 })
 
 test_that("pie.size works", {
+
+    p_size <- pie.size(p, outer = 80, inner = 50, width = 200, height = 200)
+
     sz <- p_size$x$size
     expect_equal(sz$canvasWidth, 200)
     expect_equal(sz$canvasHeight, 200)
@@ -22,11 +21,16 @@ test_that("pie.size works", {
     expect_equal(sz$pieOuterRadius, '80%')
 
     expect_is(sz, 'list')
-    expect_is(p, "pier")
-    expect_is(p, "htmlwidget")
+    expect_is(p_size, "pier")
+    expect_is(p_size, "htmlwidget")
 })
 
 test_that("pie.header works", {
+
+    p_header <- pie.header(p, text = 'My New Title', font = 'Impact',
+                           size = 18, colour = 'grey',
+                           location = 'top-center')
+
     hd <- p_header$x$header$title
     expect_equal(hd$text, 'My New Title')
     expect_equal(hd$font, 'Impact')
@@ -35,6 +39,41 @@ test_that("pie.header works", {
     expect_equal(p_header$x$header$location, 'top-center')
 
     expect_is(p_header$x$header, 'list')
-    expect_is(p, "pier")
-    expect_is(p, "htmlwidget")
+    expect_is(p_header, "pier")
+    expect_is(p_header, "htmlwidget")
+})
+
+test_that("pie.size works", {
+
+    p_subtitle <- pie.subtitle(p, text='by Type')
+
+    expect_equal(p_subtitle$x$header$subtitle$text, "by Type")
+
+    expect_is(p_subtitle, "pier")
+    expect_is(p_subtitle, "htmlwidget")
+})
+
+test_that("pie.tootips works", {
+
+    p_tootips <- pie.tooltips(p)
+
+    expect_true(p_tootips$x$tooltips$enabled)
+
+    expect_is(p_tootips, "pier")
+    expect_is(p_tootips, "htmlwidget")
+})
+
+test_that("pie.effects works", {
+
+    p_effects <- pie.effects(p, load  = list(effect = 'default',
+                                             speed = 1000))
+
+    pe <- p_effects$x$effects$load
+
+    expect_equal(pe$effect, 'default')
+    expect_equal(pe$speed, 1000)
+
+    expect_is(pe, 'list')
+    expect_is(p_effects, "pier")
+    expect_is(p_effects, "htmlwidget")
 })
